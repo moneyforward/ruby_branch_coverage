9e9100eb48e6476f083de5dbff375deee5f5c126
hiraishi981215 <hiraishi.chika@moneyforward.co.jp>
2023-05-26 10:30:15 +0900

    style: applied style format

--- lib/branchcov/xmlruby/sonar.rb
+++ lib/branchcov/xmlruby/sonar.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
+
 require 'branchcov/xmlruby/sonar/version'
 require 'branchcov/xmlruby/sonar/engine'
 
 module Branchcov
   module Xmlruby
+    # Specific execution details are described in ruby_branch_cover
     module Sonar
     end
   end
--- lib/branchcov/xmlruby/sonar/engine.rb
+++ lib/branchcov/xmlruby/sonar/engine.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
-class BranchcovXmlrubySonar::Engine < ::Rails::Engine
-  isolate_namespace BranchcovXmlrubySonar
+
+module BranchcovXmlrubySonar
+  # This engine is for dummy app
+  class Engine < Rails::Engine
+    isolate_namespace BranchcovXmlrubySonar
+  end
 end

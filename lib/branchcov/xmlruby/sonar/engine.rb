0a31581d4dd75ba2d2df079d0f9d2a9236cf0508
hiraishi981215 <hiraishi.chika@moneyforward.co.jp>
2023-05-26 17:56:12 +0900

    refactor: delete engine

--- lib/branchcov/xmlruby/sonar/engine.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module BranchcovXmlrubySonar
-  # This engine is for dummy app
-  class Engine < Rails::Engine
-    isolate_namespace BranchcovXmlrubySonar
-  end
-end
